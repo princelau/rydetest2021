eb265ac4136e2750df05ba225d65c5a0466ff1c5
prince.lau <prince.lau@circles.asia>
2021-08-20 20:29:36 +0800

    Replaced incorrect log import

--- service.go
+++ service.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bitbucket.org/libertywireless/user-service/logging"
 	"context"
+	log "github.com/Sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
@@ -17,7 +17,6 @@ type UserServiceInterface interface {
 	GetAllUsersFromDatabase(ctx context.Context) ([]User, bool, int, string)
 	DeleteUserFromDatabase(ctx context.Context, userId string) (string, bool, int, string)
 	UpdateUserFromDatabase(ctx context.Context, user *User, userId string) (string, bool, int, string)
-
 }
 
 type userService struct {
@@ -28,17 +27,16 @@ func NewUserService() UserServiceInterface {
 }
 
 func (c *userService) InsertUserIntoDatabase(ctx context.Context, user *User) (string, bool, int, string) {
-	logger := logging.GetLogger(ctx).Logger
 
 	//if user_id is empty, return 422 error as user_id will be meaningful unique identifier
 	if user.UserID == "" {
-		logger.Errorf("user_id cannot be empty")
+		log.Errorf("user_id cannot be empty")
 		return "", false, http.StatusUnprocessableEntity, `user_id cannot be empty`
 	}
 
 	//if name is empty, return 422 error as every user requires a name
 	if user.Name == "" {
-		logger.Errorf("name cannot be empty")
+		log.Errorf("name cannot be empty")
 		return "", false, http.StatusUnprocessableEntity, `name cannot be empty`
 	}
 
@@ -50,14 +48,14 @@ func (c *userService) InsertUserIntoDatabase(ctx context.Context, user *User) (s
 	findErr := collection.FindOne(ctx, searchCriteria).Decode(&duplicateUser)
 	//if something went wrong with finding user and it is not because no user found, return 500 error
 	if findErr != nil && findErr.Error() != "mongo: no documents in result" {
-		logger.Errorf(findErr.Error())
+		log.Errorf(findErr.Error())
 		return "", false, http.StatusInternalServerError, findErr.Error()
 
 	}
 
 	//if successfully found an existing user in database with user_id, return 400 error
 	if duplicateUser.UserID != "" {
-		logger.Errorf(`User with user_id: ` + duplicateUser.UserID + ` already exists in the database`)
+		log.Errorf(`User with user_id: ` + duplicateUser.UserID + ` already exists in the database`)
 		return "", false, http.StatusBadRequest, `User with user_id: ` + duplicateUser.UserID + ` already exists in the database`
 	}
 
@@ -66,7 +64,7 @@ func (c *userService) InsertUserIntoDatabase(ctx context.Context, user *User) (s
 	result, insertErr := collection.InsertOne(ctx, user)
 	//if something went wrong with inserting into database, return 500 error
 	if insertErr != nil {
-		logger.Errorf(findErr.Error())
+		log.Errorf(insertErr.Error())
 		return "", false, http.StatusInternalServerError, insertErr.Error()
 	}
 
@@ -74,18 +72,16 @@ func (c *userService) InsertUserIntoDatabase(ctx context.Context, user *User) (s
 	insertedId, ok := result.InsertedID.(primitive.ObjectID)
 	//if something went wrong with converting inserted id, return 500 error
 	if !ok {
-		logger.Errorf("failed to convert inserted id into primitive object")
+		log.Errorf("failed to convert inserted id into primitive object")
 		return "", false, http.StatusInternalServerError, "failed to convert inserted id into primitive object"
 	}
 
 	//return insert_id if everything is successful
-	logger.Debugf(`Successfully inserted: ` + user.UserID + ` into database`)
+	log.Debugf(`Successfully inserted: ` + user.UserID + ` into database`)
 	return insertedId.Hex(), true, 0, ""
 }
 
-
 func (c *userService) GetUserFromDatabase(ctx context.Context, userId string) (*User, bool, int, string) {
-	logger := logging.GetLogger(ctx).Logger
 
 	var user *User
 	collection := client.Database("database").Collection("users")
@@ -93,7 +89,7 @@ func (c *userService) GetUserFromDatabase(ctx context.Context, userId string) (*
 	searchCriteria := bson.M{"user_id": userId}
 	findErr := collection.FindOne(ctx, searchCriteria).Decode(&user)
 	if findErr != nil {
-		logger.Errorf(findErr.Error())
+		log.Errorf(findErr.Error())
 		//if user does not exist in database, return 400 error
 		if findErr.Error() == "mongo: no documents in result" {
 			return &User{}, false, http.StatusBadRequest, findErr.Error()
@@ -103,25 +99,23 @@ func (c *userService) GetUserFromDatabase(ctx context.Context, userId string) (*
 	}
 
 	//return user if everything is successful
-	logger.Debugf(`Successfully got user: ` + user.UserID + ` from database`)
+	log.Debugf(`Successfully got user: ` + user.UserID + ` from database`)
 	return user, true, 0, ""
 
 }
 
-
 func (c *userService) GetAllUsersFromDatabase(ctx context.Context) ([]User, bool, int, string) {
-	logger := logging.GetLogger(ctx).Logger
 
 	var users []User
 	collection := client.Database("database").Collection("users")
 
-	//find all users from datasbase with empty filter
+	//find all users from database with empty filter
 	cursor, findErr := collection.Find(ctx, bson.M{})
 
 	//if something went wrong with finding user, return 500 error
 	if findErr != nil {
-		logger.Errorf(findErr.Error())
-		return []User {}, false, http.StatusInternalServerError, findErr.Error()
+		log.Errorf(findErr.Error())
+		return []User{}, false, http.StatusInternalServerError, findErr.Error()
 	}
 
 	//close cursor after done
@@ -134,48 +128,47 @@ func (c *userService) GetAllUsersFromDatabase(ctx context.Context) ([]User, bool
 	}
 	//if something went wrong with cursor, return 500 error
 	if cursorErr := cursor.Err(); cursorErr != nil {
-		logger.Errorf(cursorErr.Error())
-		return []User {}, false, http.StatusInternalServerError, cursorErr.Error()
+		log.Errorf(cursorErr.Error())
+		return []User{}, false, http.StatusInternalServerError, cursorErr.Error()
 	}
 
 	//return users if everything is successful
-	logger.Debugf(`Successfully all users from database`)
+	log.Debugf(`Successfully all users from database`)
 	return users, true, 0, ""
 
 }
 
 func (c *userService) DeleteUserFromDatabase(ctx context.Context, userId string) (string, bool, int, string) {
-	logger := logging.GetLogger(ctx).Logger
 
 	collection := client.Database("database").Collection("users")
 
 	//find and delete user based on filter
 	searchCriteria := bson.M{"user_id": userId}
 	resp, deleteErr := collection.DeleteOne(ctx, searchCriteria)
-	//if something went wrong during delete, reutrn 500 error
+	//if something went wrong during delete, return 500 error
 	if deleteErr != nil {
-		logger.Errorf(deleteErr.Error())
+		log.Errorf(deleteErr.Error())
 		return "", false, http.StatusInternalServerError, deleteErr.Error()
 
 	}
 	//if user does not exist in database, return 400 error
 	if resp.DeletedCount == 0 {
-		logger.Errorf(`User with user_id: ` + userId + ` does not exist in the database`)
+		log.Errorf(`User with user_id: ` + userId + ` does not exist in the database`)
 		return "", false, http.StatusBadRequest, `User with user_id: ` + userId + ` does not exist in the database`
 	}
 	//return user_id of user if deletion is successful
-	logger.Debugf(`Successfully deleted user: ` + userId + ` from database`)
+	log.Debugf(`Successfully deleted user: ` + userId + ` from database`)
 	return userId, true, 0, ""
 }
 
 func (c *userService) UpdateUserFromDatabase(ctx context.Context, user *User, userId string) (string, bool, int, string) {
-	logger := logging.GetLogger(ctx).Logger
 
 	collection := client.Database("database").Collection("users")
 
-	//if usewr is trying to update user_id, return 400 error as it is not allowed
+	//if user is trying to update user_id, return 400 error as it is not allowed
 	if user.UserID != userId {
-		return "", false, http.StatusBadRequest, "mismatching user_id/user_id cannot be updated"
+		log.Errorf("user_id mismatch / tried to update with different user_id")
+		return "", false, http.StatusBadRequest, "user_id mismatch / tried to update with different user_id"
 	}
 
 	// find one and update using user_id as filter and update from payload as new values
@@ -183,7 +176,7 @@ func (c *userService) UpdateUserFromDatabase(ctx context.Context, user *User, us
 	_, updateErr := collection.UpdateOne(ctx, searchCriteria, bson.M{"$set": user})
 
 	if updateErr != nil {
-		logger.Errorf(updateErr.Error())
+		log.Errorf(updateErr.Error())
 		//if user does not exist in database, return 400 error
 		if updateErr.Error() == "mongo: no documents in result" {
 			return "", false, http.StatusBadRequest, updateErr.Error()
@@ -194,9 +187,7 @@ func (c *userService) UpdateUserFromDatabase(ctx context.Context, user *User, us
 	}
 
 	//return user_id of user if update is successful
-	logger.Debugf(`Successfully updated user: ` + userId + ` from database`)
+	log.Debugf(`Successfully updated user: ` + userId + ` from database`)
 	return userId, true, 0, ""
 
 }
-
-
